2cb4d4c45b9392a5453a7c09d6e617718f31523d
Akash Eldo <akasheldo@hotmail.com>
2019-01-24 19:26:36 -0500

    Initial Commit

---
+++ Assets/PositionFromSpawn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionFromSpawn : MonoBehaviour
+{
+
+    public Text text;
+    private Vector3 spawnCoord;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnCoord = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerCoord = gameObject.transform.position;
+        Vector3 displacement = playerCoord - spawnCoord;
+        text.text = displacement.ToString();
+    }
+}
