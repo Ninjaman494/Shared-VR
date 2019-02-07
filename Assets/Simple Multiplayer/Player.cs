2a2529badbf734921e05a398d545c80e33534aeb
Akash Eldo <akasheldo@hotmail.com>
2019-02-07 12:06:46 -0500

    Created a Non-VR Multiplayer Scene Each player is a cube that can be moved around with WASD and has it's own camera that follows the player around.

---
+++ Assets/Simple Multiplayer/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour {
+
+    public GameObject playerCamera;
+
+    void Start() {
+        if(isLocalPlayer == true) {
+            playerCamera.SetActive(true);
+        } else {
+            playerCamera.SetActive(false);
+        }
+    }
+
+    void Update() {
+        if(isLocalPlayer == true) {
+            if(Input.GetKey(KeyCode.D)) {
+                this.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+                this.transform.Translate(Vector3.left * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+                this.transform.Translate(Vector3.back * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.W)) {
+                this.transform.Translate(Vector3.forward * Time.deltaTime * 3f);
+            }
+        }
+    }
+}
