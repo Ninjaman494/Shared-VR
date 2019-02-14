6c78f711fe78c0182f24105b28419091d14c7149
Akash Eldo <akasheldo@hotmail.com>
2019-02-14 14:09:33 -0500

    Created VR multiplayer scene multi is a Unity scene where the User can move around and see other Users in realtime. Multiplayer requires that a Server instance is running on a computer, this computer's ip address has to be manually set in the code. The player is represented by a Cube.

---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SpatialTracking;
+
+public class PlayerScript : NetworkBehaviour {
+
+    public GameObject playerCamera;
+    public TrackedPoseDriver poseDriver;
+
+    void Start() {
+        if(isLocalPlayer == true) {
+            playerCamera.SetActive(true);
+            poseDriver.enabled = true;
+        } else {
+            playerCamera.SetActive(false);
+            poseDriver.enabled = false;
+        }
+    }
+
+    void Update() {
+        if(isLocalPlayer == true) {
+            if(Input.GetKey(KeyCode.D)) {
+                this.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.A)) {
+                this.transform.Translate(Vector3.left * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.S)) {
+                this.transform.Translate(Vector3.back * Time.deltaTime * 3f);
+            }
+            if(Input.GetKey(KeyCode.W)) {
+                this.transform.Translate(Vector3.forward * Time.deltaTime * 3f);
+            }
+        }
+    }
+}
