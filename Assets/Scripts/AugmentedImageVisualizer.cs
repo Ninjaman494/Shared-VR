dfb88926c4d1c8e75cc8dd82c14f4387971af599
Akash Eldo <akasheldo@hotmail.com>
2019-03-15 11:04:11 -0400

    Modified Visualizer to display one object Visualizer now displays a cube over the image

--- Assets/Scripts/AugmentedImageVisualizer.cs
+++ Assets/Scripts/AugmentedImageVisualizer.cs
@@ -37,7 +37,7 @@ public class AugmentedImageVisualizer : MonoBehaviour
     /// <summary>
     /// A model for the lower left corner of the frame to place when an image is detected.
     /// </summary>
-    public GameObject FrameLowerLeft;
+    public GameObject hand;
 
     /// <summary>
     /// The Unity Update method.
@@ -46,13 +46,13 @@ public class AugmentedImageVisualizer : MonoBehaviour
     {
         if (Image == null || Image.TrackingState != TrackingState.Tracking)
         {
-            FrameLowerLeft.SetActive(false);
+            hand.SetActive(false);
             return;
         }
 
         float halfWidth = Image.ExtentX / 2;
         float halfHeight = Image.ExtentZ / 2;
-        FrameLowerLeft.transform.localPosition = (halfWidth * Vector3.left) + (halfHeight * Vector3.back);
-        FrameLowerLeft.SetActive(true);
+        hand.transform.localPosition = (halfWidth * Vector3.left) + (halfHeight * Vector3.back);
+        hand.SetActive(true);
     }
 }
