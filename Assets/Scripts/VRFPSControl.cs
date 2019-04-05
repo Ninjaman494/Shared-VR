835b3da5b173b8a6947b7c9e0cf8427ca54a3933
Akash Eldo <akasheldo@hotmail.com>
2019-04-05 17:27:54 -0400

    Cleaned up WASDControl and Renamed to VRFPSControl

---
+++ Assets/Scripts/VRFPSControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SpatialTracking;
+
+public class VRFPSControl : MonoBehaviour {
+    public GameObject head;
+    public float sensitivity = 2f;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationY = 0F;
+
+    void Update() {
+        float rotationX = head.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+        head.transform.localRotation = Quaternion.Euler(-rotationY, 0, 0);
+        transform.Rotate(new Vector3(0, rotationX, 0));
+
+        if(Input.GetKey(KeyCode.D)) {
+                Debug.Log("D");
+                gameObject.transform.Translate(Vector3.right * Time.deltaTime * 3f);
+        } if(Input.GetKey(KeyCode.A)) {
+                gameObject.transform.Translate(Vector3.left * Time.deltaTime * 3f);
+        } if(Input.GetKey(KeyCode.S)) {
+                gameObject.transform.Translate(Vector3.back * Time.deltaTime * 3f);
+        } if(Input.GetKey(KeyCode.W)) {
+                gameObject.transform.Translate(Vector3.forward * Time.deltaTime * 3f);
+        }
+    }
+}
